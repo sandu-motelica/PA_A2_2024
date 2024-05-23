21bfebf95d21f571bb33ea3f36624b6486cc7751
sandu-motelica <motelica.sb@gmail.com>
2024-05-23 19:52:55 +0300

    add Swagger doc

--- Lab11/Compulsory/src/main/java/com/example/Compulsory/controller/AuthorController.java
+++ Lab11/Compulsory/src/main/java/com/example/Compulsory/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.example.Compulsory.controller;
 
 import com.example.Compulsory.model.Author;
 import com.example.Compulsory.service.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
+    @Operation(summary = "Obține lista tuturor autorilor")
     @GetMapping
     public List<Author> getAllAuthors() {
         return authorService.getAllAuthors();
